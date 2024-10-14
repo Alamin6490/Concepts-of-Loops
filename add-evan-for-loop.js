881208ff6e431c300f4f78f736527a05e84d24f5
Alamin6490 <132498750+Alamin6490@users.noreply.github.com>
2024-10-14 16:08:33 +0600

    odd-evven

---
+++ add-evan-for-loop.js
@@ -0,0 +1,18 @@
+for(let i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+
+// how to run even  number
+
+for(let i = 0; i <= 20; i=i+2){
+    console.log(i)
+}
+
+// how to run odd number
+for(let i = 1 ; i<= 50;
+    i = i + 2;
+
+){
+    console.log(i)
+}
