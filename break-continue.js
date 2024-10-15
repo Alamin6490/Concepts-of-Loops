fa08582ec69e3ac0acd6f6d9a7c2a0898adaee1d
Alamin6490 <132498750+Alamin6490@users.noreply.github.com>
2024-10-15 16:08:56 +0600

    loop

---
+++ break-continue.js
@@ -0,0 +1,17 @@
+for(let i= 1; i*i < 150; i++){
+    console.log(i*i );
+    if(i>= 100){
+        break;
+    }
+}
+console.log('life after break up');
+
+// another exmple
+// let n = 54;
+// while(n > 25){
+//     console.log(n);
+//     if(n <50){
+//         break;
+//     }
+//     n--;
+// }
---
+++ decremental.js
@@ -0,0 +1,8 @@
+// ddecremental fo loop
+for(let i = 110; i<=10; i--){
+    console.log(i)
+}
+
+for (let i = 20; i>=11; i--){
+    console.log(i);
+}
